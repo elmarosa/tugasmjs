145f9df3aecb93c73af9cb3afbd0c8bd550e9ca8
elmarosa <114921856+elmarosa@users.noreply.github.com>
2023-12-30 01:15:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  const stream = fs.createReadStream('./index.html');
+  const filePath = path.join(__dirname, 'static', 'index.html');
+  const stream = fs.createReadStream(filePath);
+  
+  stream.on('error', (err) => {
+    console.error(`Error reading file: ${err.message}`);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  });
+  
   stream.pipe(res);
 });
 
